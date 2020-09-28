f4e37b6bfa645dbcbcaf7adf098fc52ab2c47712
Toni <44346865+ToniaSanzo@users.noreply.github.com>
2020-09-28 10:34:34 -0400

    Update FibonacciHeap.h

--- FibonacciHeap.h
+++ FibonacciHeap.h
@@ -18,12 +18,12 @@
     File: FibonacciHeap.h
     Author: Tonia Sanzo (sanzo.tonia@gmail.com)
 
-    A fibonacci heap implemented as descriped by Kevin Wayne
+    A fibonacci heap implemented as described by Kevin Wayne
     (wayne@cs.princeton.edu)
     www.cs.princeton.edu/~wayne/teaching/fibonacci-heap.pdf
 
     Fibonacci heap was invented by Fredman and Tarjan, 1986.
-    I did extensive QA on this program, but if you do discover
+    I did extensive QA on this program, but if you discover
     a bug please contact me at <sanzo.tonia@gmail.com>
 
     ______________________________________________________________
